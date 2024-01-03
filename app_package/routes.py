58082b192525710535cb85431aa5e5629030b3c9
tpayne <tpayne@Xubuntu.myguest.virtualbox.org>
2024-01-02 18:11:59 -0600

    feat: add variables to pass to index.html webpage

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,17 +1,9 @@
+from flask import render_template
 from app_package import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : 'Tyler Payne'}
-    return '''
-    <html>
-        <head>
-        <title> Home Page - Microblog </title>
-        </head>
-        <body>
-            <h1> Hello, ''' + user['username'] + '''!</h1>
-        </body>
+    return render_template('index.html', title='Home', user=user)
     
-    
-    </html>'''
