7cfd760be6f837837a8ea5dfc66ecfedd5160560
tpayne <tpayne@Xubuntu.myguest.virtualbox.org>
2024-01-02 18:48:56 -0600

    feat: added for loop to render blog posts in html.

--- app_package/routes.py
+++ app_package/routes.py
@@ -5,5 +5,24 @@ from app_package import app
 @app.route('/index')
 def index():
     user = {'username' : 'Tyler Payne'}
-    return render_template('index.html', user=user)
+    posts = [
+            {
+                'author': {'username': 'John'},
+                'body': 'Beautiful day in Portland!'
+            },
+            {
+                'author': {'username': 'Susan'},
+                'body': 'The Avengers movie was so cool!'
+            },
+            {
+                'author': {'username': 'Tyler'},
+                'body': 'Learning a lot today!'
+            },
+            {
+                'author': {'username': 'John Honnold'},
+                'body': 'I am such a good boss!'
+            }
+
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
     
