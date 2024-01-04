3762f535a8149dd9e426ec3aa029fd8284cf6a6d
tpayne <tpayne@Xubuntu.myguest.virtualbox.org>
2024-01-03 19:04:49 -0600

    feat: add logic for processing login form data
    Added the code for the login view to process data that the user
    submitted using the form in login.html

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app_package import app
 from app_package.forms import LoginForm
 
@@ -30,4 +30,8 @@ def index():
 @app.route('/login')
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
